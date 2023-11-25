3e2fe0831916f3614e80784b17062b134407bfa5
chainsx <i@mail.chainsx.cn>
2023-11-26 02:46:12 +0800

    target: add support for tinyvision

---
+++ target/linux/yuzukihd/image/v851se.mk
@@ -0,0 +1,7 @@
+define Device/yuzuki_tinyvision
+  DEVICE_VENDOR := YuzukiHD
+  DEVICE_MODEL := TinyVision
+  DEVICE_DTS = sun8i-v851se-tinyvision
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | sunxi-img | gzip | append-metadata
+endef
+TARGET_DEVICES += yuzuki_tinyvision
---
+++ target/linux/yuzukihd/v851se/target.mk
@@ -0,0 +1,5 @@
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=YuzukiHD TinyVision
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
