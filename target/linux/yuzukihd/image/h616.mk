d03d8bec2e8b39e8461a617200876dd627b7f956
chainsx <i@mail.chainsx.cn>
2024-01-03 19:38:39 +0800

    yuzukihd: add h616 patch

--- target/linux/yuzukihd/image/h616.mk
+++ target/linux/yuzukihd/image/h616.mk
@@ -2,7 +2,7 @@ define Device/yuzuki_chameleon
   KERNEL_NAME := Image
   DEVICE_VENDOR := YuzukiHD
   DEVICE_MODEL := Chameleon
-  DEVICE_DTS = allwinner/sun50i-h616-biqu
+  DEVICE_DTS = allwinner/sun50i-h616-yuzukichameleon
   IMAGE/sysupgrade.img.gz := uboot-img | gzip | append-metadata
 endef
 TARGET_DEVICES += yuzuki_chameleon
